7409b795adf687aa3f74973208273f81f5c00003
Silas Wisdom <silaswisdom5@gmail.com>
2025-11-12 17:07:07 +0100

    updated coingecko_service.dart

--- lib/services/coingecko_service.dart
+++ lib/services/coingecko_service.dart
@@ -8,6 +8,7 @@ class CoinGeckoService {
   static Future<List<Coin>> fetchTopCoins({int perPage = 50, int page = 1}) async {
     final url =
         '$_base/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=$perPage&page=$page&sparkline=false&price_change_percentage=24h';
+        await Future.delayed(const Duration(milliseconds: 100));
     final res = await http.get(Uri.parse(url));
     if (res.statusCode != 200) throw Exception('Failed fetching coins');
     final List data = json.decode(res.body);
